1443dba13c5170a6ad7da9402fd76b22e880ff89
Shantanu Chatterjee <cshantanu712@gmail.com>
2023-04-17 16:19:51 +0530

    17 Apr, 2023

--- SpecialSumOfArray.java
+++ SpecialSumOfArray.java
@@ -7,7 +7,7 @@ public class Solution{
 		for(int i : arr) {
 			sum += i;
 		}
-		while(sum > 9) {
+		while(sum > 9) { //sum until it gets a one digit sum
 			int sumOfSum = 0;
 			while(sum > 0) {
 				int rem = sum %10;
