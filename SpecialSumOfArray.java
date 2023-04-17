4f642d152dd54c85af36b7436e26a9fdecee8950
Shantanu Chatterjee <cshantanu712@gmail.com>
2023-04-17 16:18:35 +0530

    c1

---
+++ SpecialSumOfArray.java
@@ -0,0 +1,21 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution{
+	public static int specialSum(ArrayList<Integer> arr, int n ){
+        // Write your code here.
+		int sum = 0;
+		for(int i : arr) {
+			sum += i;
+		}
+		while(sum > 9) {
+			int sumOfSum = 0;
+			while(sum > 0) {
+				int rem = sum %10;
+				sumOfSum += rem;
+				sum /= 10;
+			}
+			sum = sumOfSum;
+		}
+		return sum;
+	}
+}
