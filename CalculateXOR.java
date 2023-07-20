c909fe6f5b9b4d79cd91da02e76a2443d7a2d8e7
Shantanu Chatterjee <cshantanu712@gmail.com>
2023-07-20 13:02:24 +0530

    interview prep

---
+++ CalculateXOR.java
@@ -0,0 +1,18 @@
+public class Solution {
+	public static int calculateXOR(int n) {
+		// Write your code here.
+		if (n % 4 == 0)
+            return n;
+      
+        // If n%4 gives remainder 1
+        if (n % 4 == 1)
+            return 1;
+      
+        // If n%4 gives remainder 2
+        if (n % 4 == 2)
+            return n + 1;
+      
+        // If n%4 gives remainder 3
+        return 0;
+	}
+}
