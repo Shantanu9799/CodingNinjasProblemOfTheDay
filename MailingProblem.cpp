acb2d9d23c3f905873c5105b1f28a37bbaf05be4
Shantanu9799 <cshantanu712@gmail.com>
2024-05-14 19:44:12 +0530

    mailing problem optimised answer

--- MailingProblem.cpp
+++ MailingProblem.cpp
@@ -1,3 +1,20 @@
+int timeTakenToMail(string keyboard, string s) {
+    //    Write your code here
+    int keyboardIndex[26] = {0};
+    // storing the index of keyboard character
+    for(int i = 0; i < keyboard.size(); ++i) {
+        keyboardIndex[keyboard[i] - 'a'] = i;
+    }
+    // we have to start from the 0th index
+    int idx = 0, time = 0;
+    for(int i = 0; i < s.size(); ++i) {
+        time += abs(keyboardIndex[s[i] - 'a'] - idx);
+        idx = keyboardIndex[s[i] - 'a'];
+    }
+    return time;
+}
+
+
 #include <bits/stdc++.h>
 int timeTakenToMail(string keyboard, string s) {
     //    Write your code here
